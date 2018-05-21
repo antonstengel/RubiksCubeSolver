ed64010659a65017dee6c66d74b3ec507eb9c01f
antonstengel <31658804+antonstengel@users.noreply.github.com>
2018-05-20 21:08:02 -0400

    i did a ton of shit i need to get a life

--- sketch.js
+++ sketch.js
@@ -46,12 +46,14 @@ console.log(tree._root);
 */
 
 //defines numbers as 6 sides of the cube
-var front = 0; //test for henry
-var up = 1;
-var down = 2;
+
+
+var front = 0;
+var up = 4;
+var down = 5;
 var right = 3;
-var left = 4;
-var back = 5;
+var left = 1;
+var back = 2;
 var cube = new Array(6);
 
 
@@ -67,34 +69,32 @@ function setup() {
       }
    }
 
-   randomize(cube);
+   //randomize(cube);
+   solvedState(cube);
+
+   L(cube);
+   R(cube);
+   B(cube);
+   Ri(cube);
+   D(cube);
+   R(cube);
+   Bi(cube);
+   R(cube);
+   L(cube);
+   Di(cube);
+   U(cube);
+
+
+
 
    solveCross(cube);
-
    solveCorners(cube);
+   solveMiddle(cube);
+
 
 
 
 } //setup
-
-
-
-function solveCorners(cube) {
-   var count = 0;
-
-
-   if (cube[up][0][0] == 'W')
-   if (cube[up][2][0] == 'W')
-   if (cube[up][0][2] == 'W')
-   if (cube[up][2][2] == 'W')
-
-
-   console.log(count);
-}
-
-
-
-
 
 
 
@@ -107,59 +107,5 @@ function solveCorners(cube) {
 //////////DRAW//////////
 ////////////////////////
 function draw() {
-   //visuales cube layout on screen
-   for (var s = 0; s < 6; s++) {
-      for (var x = 0; x < 3; x++) {
-         for(var y = 0; y < 3; y++) {
-            //print(cube[s][y][z]); //prints location of cube
-
-            var bigX = 0; //x-location of sides
-            var smallX = 0; //x-location of individual piece
-            var bigY = 0; //y-location of sides
-            var smallY = 0; //y-location of individual piece
-
-            if(s == front){
-               bigX = 150;
-               bigY = 150;
-            }
-            else if(s == up){
-               bigX = 150;
-               bigY = 0;
-            }
-            else if(s == down){
-               bigX = 150;
-               bigY = 300;
-            }
-            else if(s == right){
-               bigX = 300;
-               bigY = 150;
-            }
-            else if(s == left){
-               bigX = 0;
-               bigY = 150;
-            }
-            else{
-               bigX = 450;
-               bigY = 150;
-            }
-
-            if (x == 0) smallX = 0;
-            else if (x == 1) smallX = 50;
-            else smallX = 100;
-
-            if (y == 0) smallY = 0;
-            else if (y == 1) smallY = 50;
-            else smallY = 100;
-
-            strokeWeight(6);
-            if (cube[s][x][y] == 'B') fill(0,0,255);
-            else if (cube[s][x][y] == 'Y') fill(255,255,0);
-            else if (cube[s][x][y] == 'W') fill(255,255,255);
-            else if (cube[s][x][y] == 'R') fill(255,0,0);
-            else if (cube[s][x][y] == 'O') fill(255,150,0);
-            else if (cube[s][x][y] == 'G') fill(0,250,0);
-            rect(smallX + bigX, smallY + bigY, 50, 50, 8);
-         } //y
-      } //x
-   } //s
+   visualize(cube);
 } //draw
